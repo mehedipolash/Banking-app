38f16274d4b789be420802f63eff09be80eab02c
mehedipolash <mehedipolash228@gmail.com>
2025-03-15 02:09:02 +0600

    completed

---
+++ script/login.js
@@ -0,0 +1,22 @@
+document
+  .getElementById('login-btn')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+     const accountNumber = document.getElementById('account-number').value;
+
+     const pinNumber = document.getElementById('pin').value;
+     const convertedPinNumber = parseInt(pinNumber);
+
+    
+
+    if (accountNumber.length === 11) {
+      if (convertedPinNumber === 1234) {
+        window.location.href = 'main.html';
+      } else {
+        alert('Pin thik nai!');
+      }
+    } else {
+      alert('accountnumber thik nai');
+    }
+  });
